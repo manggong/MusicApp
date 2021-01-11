3cf72592ab2f89566e861b0c0f4e745948f44c4d
manggong <jihwank1995@gmail.com>
2021-01-11 19:05:38 +0900

    add TrackManager properties

--- AppleMusicStApp/Track/TrackManager.swift
+++ AppleMusicStApp/Track/TrackManager.swift
@@ -10,9 +10,12 @@ import UIKit
 import AVFoundation
 
 class TrackManager {
-    // TODO: 프로퍼티 정의하기 - 트랙들, 앨범들, 오늘의 곡
     
-    // TODO: 생성자 정의하기
+    var tracks: [AVPlayerItem] = []
+    var albums: [Album] = []
+    var todaysTrack:AVPlayerItem?
+     
+    
     init() {
     }
 
